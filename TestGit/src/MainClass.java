5ae797a04d93c17584e2f3d7beccd182a49cd6de
Loïc <Loc@DESKTOP-GOBOUMG>
2018-03-09 11:27:03 +0100

    version 1

---
+++ TestGit/src/MainClass.java
@@ -0,0 +1,14 @@
+
+public class MainClass 
+{
+
+	public static void main(String[] args) 
+	{
+
+		TestGit update = new TestGit();
+
+		
+		System.out.println(update.returnTheString());
+	}
+
+}
--- TestGit/src/TestGit.java
+++ TestGit/src/TestGit.java
@@ -2,5 +2,20 @@
 public class TestGit 
 {
 
-	private String gitFromScratch;
+	private String gitFromScratch ="Zebi";
+	
+	public TestGit(String entre)
+	{
+		this.gitFromScratch = entre;
+	}
+	
+	public TestGit()
+	{
+		
+	}
+	
+	public String returnTheString()
+	{
+		return this.gitFromScratch;
+	}
 }
